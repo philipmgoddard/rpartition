6a2437d18659b1e20ec738af2c434f0b97309993
Philip Goddard <pmgoddard89@gmail.com>
2016-01-24 11:56:44 +0000

    continued, some bug fix

--- rpart.R
+++ rpart.R
@@ -1,7 +1,7 @@
+# TO DO- at terminal nodes, write out entire set of rules
+# from there, think about how to implement- eg this just splits,
+# but as it stands cannot use for prediction
 
-# make a wrapper function for recursivePart
-depth <- -1 
-node_info <- vector(mode = "list")
 
 recursivePart <- function(pred, y, min_grp = 10) {
   npred <- ncol(pred)
@@ -16,7 +16,7 @@ recursivePart <- function(pred, y, min_grp = 10) {
     for (j in 1:(len-1)) {
       grp1 <- arry[1:j]
       grp2 <- arry[(j+1):len]
-      sse <- sum((grp1 - mean(grp1)) ^ 2) + sum((grp2 - mean(grp1)) ^ 2)
+      sse <- sum((grp1 - mean(grp1)) ^ 2) + sum((grp2 - mean(grp2)) ^ 2)
       #cat("\ngrp1", length(grp1))
       #cat("\ngrp2", length(grp2))
       #cat("\nsse", sse)
@@ -33,10 +33,13 @@ recursivePart <- function(pred, y, min_grp = 10) {
     }
   }
   cat("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-  cat("\nPartition:")
-  cat("\nsse=", sseBest,
-      "\ncol index=", colInd,
-      "\nvalue=", value, "\n")
+  cat("\nDepth =", depth,
+      "\nPartition:",
+      "\nnum predictors = ", nrow(pred),
+      "\ncol name =", names(pred)[colInd],
+      "\nvalue =", value,
+      "\nsse =", sseBest,
+      "\ncol index =", colInd, "\n")
   
   #split groups
   S <- which(pred[, colInd] > value)
@@ -44,34 +47,45 @@ recursivePart <- function(pred, y, min_grp = 10) {
   group1y <- y[S]
   group2x <- pred[-S, , drop = FALSE]
   group2y <- y[-S]
-  
+
   cat("\ngrp1\n")
-  cat("nrow =", nrow(group1x), "\n")
+  cat("num pred =", nrow(group1x), "\n")
   print(cbind(group1y, group1x))
   cat("\ngrp2\n")
-  cat("nrows =", nrow(group2x), "\n")
+  cat("num pred =", nrow(group2x), "\n")
   print(cbind(group2y, group2x))
   
   depth <<- depth + 1
+  #node_info[depth] <<- depth
+  #node_info[depth][[1]] <<- sseBest
 
-  # clunky but i think you need the 4 statements
-  # due to recursive nature
+  # rememeber depth where both split (not into terminal nodes)
+  if(nrow(group1x) > min_grp & nrow(group2x) > min_grp) depth_mem <<- depth
+  
+  # clunky but i think you need several statements
+  # due to recursive nature... investigate
   if(nrow(group1x) <= min_grp)  {
     cat("\nterminal node:",
         "\ndepth =", depth,
-        "\nsplit column index= ", colInd,
-        "\nsplit val= ", value,
-        "\nsse = ", sseBest,
+        "\ncondition:",names(pred)[colInd], "GT", value,
+        #"\nsplit column index= ", colInd,
+        #"\nsplit val= ", value,
+        #"\nsse = ", sseBest,
         "\nmean node y value =", mean(group1y), "\n")
   }
   if(nrow(group2x) <= min_grp)  {
     cat("\nterminal node:",
         "\ndepth =", depth,
-        "\nsplit column index= ", colInd,
-        "\nsplit val= ", value,
-        "\nsse = ", sseBest,
+        "\ncondition:",names(pred)[colInd], "LE", value,
+        #"\nsplit column index= ", colInd,
+        #"\nsplit val= ", value,
+        #"\nsse = ", sseBest,
         "\nmean node y value =", mean(group2y), "\n")
   }
+  
+  # reset the depth memory to where both groups split without terminal node
+  if(nrow(group2x) <= min_grp & nrow(group1x) <= min_grp) depth <<- depth_mem 
+  
   if(nrow(group1x) > min_grp) {
     recursivePart(group1x, group1y, min_grp = 10)
   } 
@@ -82,6 +96,18 @@ recursivePart <- function(pred, y, min_grp = 10) {
 }
 
 ### test
-recursivePart(mtcars[, 3:7], mtcars[, 1], min_grp = 10)
+
+rPartPhil <- function(inputPred, inputOut, min_split = 10, ...) {
+  depth <<- 0 
+  path <- c("")
+  node_info <<- list()
+  depth_mem <<- NULL
+  
+  recursivePart(inputPred, inputOut, min_split)
+  
+}
+
+
+rPartPhil(mtcars[, 3:7, drop = FALSE], mtcars[, 1], min_grp = 10)
 
 
